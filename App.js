7440af2ea5fcd6c844f154b9e650895cb914a59d
Jinesh87 <zodiacaus@gmail.com>
2025-01-15 13:30:40 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { UnitSDK,UNEnvironment,UNWhiteLabelAppComponent } from 'react-native-unit-components';
+import { useFonts } from 'expo-font';
+const THEME = "";
+const LANGUAGE = "";
+const CUSTOM_FONTS = {
+  OpenSans: 'OpenSans-Regular',
+};
+const jwtToken = "jwtTocken Here";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'), // Load OpenSans-Regular font
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      // Initialize Unit SDK only after fonts are loaded
+      UnitSDK.init(UNEnvironment.sandbox, THEME, LANGUAGE, CUSTOM_FONTS);
+    }
+  }, [fontsLoaded]);
+
+  return (
+    <View style={styles.container}>
+      {/* <Text>Hello World!!!</Text> */}
+      <UNWhiteLabelAppComponent jwtToken={jwtToken} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,41 @@
+# React Native App with Unit SDK Integration
+
+This project demonstrates how to integrate the Unit SDK into a React Native application, with custom fonts and other configurations. The app uses the `react-native-unit-components` library and Expo for font management.
+
+
+## Versions
+- **Node**: 22.11.0
+- **React**: 18.3.1
+- **React-Native** : 0.76.6
+
+## Features
+
+- Integration of Unit SDK with custom fonts.
+- Use of `expo-font` for managing and loading fonts.
+- A basic `UNWhiteLabelAppComponent` to display Unit components.
+- JWT token-based authentication.
+
+## Prerequisites
+
+Before running the application, ensure you have the following installed:
+
+- Node.js
+- Expo CLI
+- React Native development environment set up ([React Native Getting Started](https://reactnative.dev/docs/environment-setup))
+- A valid JWT token and API keys for Unit SDK.
+
+## Installation and Setup Instructions 
+
+Clone down this repository. You will need `node` and `npm` installed globally on your machine.  
+
+Installation:
+
+`npm install`   
+
+To Start Server:
+
+`npm start`  
+
+To Visit App:
+
+Scan the QR code with Expo Go (Android) or the Camera app (iOS)
